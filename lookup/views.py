fec45f5a5effcc5fefe947bb03e39f3eb5f8e103
pedram farjadi <pedramfarjadi@gmail.com>
2021-09-05 10:41:08 +0430

    took the api from airnow and send the json to home page

--- lookup/views.py
+++ lookup/views.py
@@ -2,7 +2,18 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    import json
+    import requests
+
+    api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=06938BC6-81EF-405A-AFA0-4DA5AA27E1D7")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."    
+
+    #https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=25&API_KEY=06938BC6-81EF-405A-AFA0-4DA5AA27E1D7
+    return render(request, 'home.html', {"api": api})
 
 def about(request):
     return render(request, 'about.html', {})
